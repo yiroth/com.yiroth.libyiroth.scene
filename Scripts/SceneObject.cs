e2c647e13cf9c4903205d3e8ddc1fe9de30ebdc6
Alsiel Nokklair <alsielnokklair@gmail.com>
2025-12-12 13:46:09 +0300

    Scene package

---
+++ Scripts/SceneObject.cs
@@ -0,0 +1,53 @@
+using LibYiroth;
+using UnityEngine;
+
+namespace LibYiroth.Scene
+{
+    public enum EStates
+    {
+        None,
+        Activated,
+        Deactivated,
+    }
+    
+    public class SceneObject : MonoBehaviour, Save.ISaveGame
+    {
+        public Data.Identification levelID = new(Data.IdentificationCategories.Game, Data.IdentificationTypes.Level, "");
+        public Data.Node node;
+        public EStates state = EStates.None;
+        
+        private Save.SaveManager _saveManager;
+
+        private void Start()
+        {
+            _saveManager = FindFirstObjectByType<Save.SaveManager>();
+
+            if (_saveManager == null)
+                return;
+        }
+
+        public bool OnSaving(Save.SaveManager saveManager)
+        {
+            if (saveManager == null)
+            {
+                Debug.LogError("Save Manager not found!");
+                return false;
+            }
+
+            if (saveManager.GetActiveGameSlot() == null)
+            {
+                Debug.LogError("Game Slot not found!");
+                return false;
+            }
+
+            saveManager.SaveVariable(levelID, "animekizi", transform.localPosition);
+            
+            return true;
+        }
+
+        public bool OnLoading(Save.SaveManager saveManager)
+        {
+            return false;
+        }
+    }
+}
